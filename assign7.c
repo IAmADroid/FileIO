e0fdb4dd5d654ecec9a85239dc2b13338f521fa1
ImAmARobot <funnygeeks101@gmail.com>
2019-11-24 22:44:58 -0600

    Final result

--- assign7.c
+++ assign7.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <ctype.h>
+#include <string.h>
+
 
 //TODO: move definitions to header file.
 
 #define ERR_COURSE_NOT_FOUND "ERROR: course not found"
+#define ERR_COURSE_EXISTS "ERROR: course already exists"
+#define ERR_INVALID_OPTION "ERROR: invalid option"
 
 typedef struct
 {
@@ -28,6 +32,25 @@ COURSE* findEntry(int courseNum);
 void writeEntry(int courseNum, COURSE* course);
 void printCourse(COURSE* c);
 
+//String editing methods
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+char *trim(char *s)
+{
+    return rtrim(ltrim(s)); 
+}
 
 //Console input methods
 int readNum(char* prompt){
@@ -41,13 +64,23 @@ int readNum(char* prompt){
     errno = 0;
 
     long num = strtol(buffer, &endptr, 10);
-    //TODO; handle case where a user types "0"
-    //maybe with sscanf?
-    if(errno == 0 && num != 0){
+
+    int noNumberRead = 0;
+    //From strtol's documentation:
+    // If the subject sequence is empty or does not have the expected form,
+    // no conversion is performed;
+    // the value of nptr shall be stored in the object pointed to by endptr,
+    // provided that endptr is not a null pointer.
+    if(buffer == endptr){
+      noNumberRead = 1;
+    }
+
+
+    if(errno == 0 && !noNumberRead){
       return (int) num;
     }
 
-    printf("Num: %ld, errno=%d, end?\"%s\"\n", num, errno, endptr);
+    printf("Invalid number.\n");
   }
 }
 
@@ -62,7 +95,7 @@ int main(void){
     char option = menuSelect();
 
     if(feof(stdin)){
-      printf("Exiting...");
+      end();
       return(0);
     }
 
@@ -79,12 +112,14 @@ int main(void){
     case 'd':
       deleteCourse();
       break;
+    case 'x':
+      printf("xkcd, Elements of Java Style, Clinic of Horrors (webtoon), Tomboys (girls that do cool stuff like dirtbike racing), Hackers (movie).\nC is terrible for console input, but it feels very rewarding for some reason?\n");
     default:
-      printf("Unexpected selection: \"%c\"\n", option);
+      fprintf(stderr, "%s\n", ERR_INVALID_OPTION );
       break;
     }
 
-
+    printf("\n");
   }
 
   end();
@@ -95,7 +130,7 @@ int main(void){
  * and writeEntry.
  */
 void start(){
-  FILE* file = fopen("./courses.dat", "a+");
+  FILE* file = fopen("./courses.dat", "r+");
   if(file == NULL){
     fprintf(stderr, "Could not open file.");
   }
@@ -106,6 +141,7 @@ void start(){
  * closes data file
  */
 void end(){
+  printf("Exiting...");
   FILE* file = a7datafile; //retrieve global variable
   fclose(file);
 }
@@ -140,6 +176,8 @@ void writeEntry(int courseNum, COURSE* course){
   if(numWrite != 1){
     fprintf(stderr, "ERROR: Could not write record.");
   }
+
+  fflush(file);
 }
 
 void printCourse(COURSE* c){
@@ -150,35 +188,56 @@ void printCourse(COURSE* c){
 }
 
 void createCourse(){
-  //TODO: CreateCourse method.
-
   printf("Please enter course details.\n");
 
   int courseNum = readNum("Course number: ");
-  //TODO: check if course already exists
+
+  COURSE* c = findEntry(courseNum);
+  if(c != NULL){
+    fprintf(stderr, "%s\n", ERR_COURSE_EXISTS);
+    return;
+  }
+
+  COURSE course;
+
 
   printf("Name of the course: ");
   char courseName[128];
   fgets(courseName, 128, stdin);
 
+  if(courseName[strlen(courseName) - 1] == '\n'){
+    courseName[strlen(courseName) - 1] = '\0';
+  }
+
+  memcpy(course.courseName, courseName, 63);
+  course.courseName[63] = '\0';
+
   printf("Course schedule: ");
   char buffer[128];
   fgets(buffer, 128, stdin);
-  char courseSchedule[4];
+
+  if(buffer[strlen(buffer) - 1] == '\n'){
+    buffer[strlen(buffer) - 1] = '\0';
+  }
+
+  memcpy(course.courseSched, buffer, 3);
+  course.courseSched[3] = '\0';
 
   int creditHours = readNum("Credit hours: ");
+  course.courseHours = creditHours;
 
   int enrollment = readNum("Enrollment (students already in the course): ");
-
-
-  //TODO: write course to file.
+  course.courseSize = enrollment;
+  
+  printCourse(&course);
+  writeEntry(courseNum, &course);
 
 }
 void readCourse(){
   int courseNum = readNum("Enter a CS course number: ");
   COURSE* course = findEntry(courseNum);
   if(course == NULL){
-    printf("%s\n", ERR_COURSE_NOT_FOUND );
+    fprintf(stderr, "%s\n", ERR_COURSE_NOT_FOUND );
     return;
   }
 
@@ -186,14 +245,109 @@ void readCourse(){
   printCourse(course);
   free(course);
 }
-void updateCourse(){}
-void deleteCourse(){}
+void updateCourse(){
+  int courseNum = readNum("Enter a CS course number: ");
+  COURSE* course = findEntry(courseNum);
+  if(course == NULL){
+    fprintf(stderr, "%s\n", ERR_COURSE_NOT_FOUND );
+    return;
+  }
+
+  printf("Type a new value, or leave the new value blank ");
+  printf("to maintain original value.\n");
+  
+  printf("Course name: \"%s\"\n", course->courseName);
+  printf("type new value: ");
+
+  char buffer[128];
+  char* trimmed;
+
+  fgets(buffer, 128, stdin);
+  trimmed = trim(buffer);
+  if(strlen(trimmed) != 0){
+    memcpy(course->courseName, trimmed, 63);
+    course->courseName[63] = '\0';
+    printf("Edited.\n"); 
+  } else {
+    printf("Retaining original value.\n");
+  }
+
+  printf("Scheduled days: \"%s\"\n", course->courseSched);
+  printf("type new value: ");
+
+  fgets(buffer, 128, stdin);
+  trimmed = trim(buffer);
+  if(strlen(trimmed) != 0){
+    memcpy(course->courseSched, trimmed, 3);
+    course->courseSched[3] = '\0';
+    printf("Edited.\n");
+  } else {
+    printf("Retaining original value.\n");
+  }
+
+  printf("Credit hours: \"%d\"\n", course->courseHours);
+  printf("type new value: ");
+
+  fgets(buffer, 128, stdin);
+  trimmed = trim(buffer);
+  if(strlen(trimmed) != 0){
+    errno = 0;
+    char* endptr;
+    long num = strtol(trimmed, &endptr, 10);
+
+    if(errno == 0 && trimmed != endptr){
+      course->courseHours = num;
+    }
+
+    printf("Edited.\n");
+  } else {
+    printf("Retaining original value.\n");
+  }
+
+  printf("Enrolled Students: \"%d\"\n", course->courseSize);
+  printf("type new value: ");
+
+  fgets(buffer, 128, stdin);
+  trimmed = trim(buffer);
+  if(strlen(trimmed) != 0){
+    errno = 0;
+    char* endptr;
+    long num = strtol(trimmed, &endptr, 10);
+
+    if(errno == 0 && trimmed != endptr){
+      course->courseSize = num;
+    }
+
+
+    printf("Edited.\n");
+  } else {
+    printf("Retaining original value.\n");
+  }
+
+  writeEntry(courseNum, course);
+
+}
+void deleteCourse(){
+  //Prompt user for selection
+  int courseNum = readNum("Enter a course number: ");
+
+  //Find selection
+  COURSE* course = findEntry(courseNum);
+  if(course == NULL){
+    fprintf(stderr, "%s\n", ERR_COURSE_NOT_FOUND);
+    return;
+  }
+
+  //Delete selection
+  course->courseHours = 0;  //No course has 0 credit hours
+                            //So this is interpreted as a
+                            //deleted course.            
+  writeEntry(courseNum, course);
+  printf("%d was successfully deleted.\n", courseNum);
+}
 
 
 char menuSelect(){
-  //TODO: Exit program if CTRL-D is entered
-  //TODO: Loop input selection untill valid input is selected.
-  
   //Prompt User
   printf("Enter one of the following actions or press CTRL-D to exit.\n");
   printf("C - create a new course record\n");
@@ -205,7 +359,13 @@ char menuSelect(){
   char buffer[128];
   fgets(buffer, 128, stdin);
 
-  //TODO: Verify that a valid menu option was chosen
+  if(strcmp(buffer, "best\n") == 0){
+    return 'x';
+  }
+
+  if(buffer[0] == 'x'){
+    return 'y';
+  }
 
   //Return character
   buffer[0] = tolower(buffer[0]);
